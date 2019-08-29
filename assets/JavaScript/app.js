53e8c229e3afe79adef75ccd900cb39c0f4ef150
jon alden <jondevalden@gmail.com>
2019-08-29 16:32:36 -0500

    Working on run tracker functionality

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -5,7 +5,7 @@ let perPage = 9;
 let offset = 0;
 const userInput = document.getElementById("button");
 
-const quotes = ['"Run when you can, walk if you must, crawl if you have to, just never give up"', '"We are what we repeatedly do. Excellence, then, is not an act, but a habit"', '"One run can change your day. Many runs can change your life"', '"The miracle isn’t that I finished. The miracle is that I had the courage to start"'];
+const quotes = ['"The voice inside your head that says this is impossible is a liar"', '"Run when you can, walk if you must, crawl if you have to, just never give up"', '"We are what we repeatedly do. Excellence, then, is not an act, but a habit"', '"One run can change your day. Many runs can change your life"', '"The miracle isn’t that I finished. The miracle is that I had the courage to start"'];
 
 
 
@@ -29,6 +29,8 @@ document.getElementById("button").addEventListener("click", function (event) {
 
     // display hidden weather table
     document.getElementById("weatherDiv").style.display = "block";
+    document.getElementById("motivationalQuote").style.display = "block";
+    document.getElementById("beerDiv").style.display = "block";
 
     //if statement to run the functions in the click listener 
     if (offset === 0) {
@@ -44,22 +46,22 @@ document.getElementById("button").addEventListener("click", function (event) {
 
 
 
-    //function for getting music from Napster
+    //function for getting quotes from our array and displayin them on the page
     function getQuotes() {
 
-        //grabbing motivational quote from the dom and changing the HTML to the quotes in pour array
+        //grabbing motivational quote from the dom and changing the HTML to the quotes in our array
         const quoteDiv = document.getElementById('motivationalQuote');
-        quoteDiv.innerHTML = '"The voice inside your head that says you can’t do this is a liar"';
-        let counter = 0;
+
+        //start at the second quote in the array
+        let counter = 1;
+
+        //display first quote of the array when page is loaded
+        quoteDiv.innerHTML = quotes[0];
 
         //function to cycle through the quotes in the array without repeating the quote twice in a row
-        function nextWord() {
-            quoteDiv.innerHTML = quotes[counter % quotes.length];
-            counter += 1;
-        }
-
-        //setting interval to display a new quote every 7 seconds
-        setInterval(nextWord, 7000);
+        setInterval(function() {
+            quoteDiv.innerHTML = quotes[counter++ % quotes.length];
+          }, 8000)
     }
 
 
@@ -67,10 +69,8 @@ document.getElementById("button").addEventListener("click", function (event) {
     //function for getting weather data
     function getweather() {
 
-
         //variable containing the Open Weather URL
         const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherKey;
-        console.log(weatherURL);
 
         // Get function using Axios to call the response of Open Weather
         axios.get(weatherURL).then(function (response) {
@@ -80,10 +80,11 @@ document.getElementById("button").addEventListener("click", function (event) {
             weatherDiv = document.createElement("div");
             weatherDiv.classList.add("weatherDisplay");
 
+            let icon = document.getElementById("weatherDescription").textContent = response.data.weather[0].icon;
             document.getElementById("weather").textContent = Math.floor(response.data.main.temp - 273.15) * 9 / 5 + 32 + " " + "Degrees ";
             document.getElementById("weatherDescription").textContent = response.data.weather[0].description;
             document.getElementById("humidity").textContent = response.data.main.humidity + "%";
-            document.getElementById("city").textContent = "Running fast in " + " " + response.data.name;
+            document.getElementById("city").innerHTML = "Current weather in " + " " + response.data.name + " " + '<img src="http://openweathermap.org/img/wn/' + icon + '.png" width="150" height="150"/>';
             document.getElementById("wind").textContent = Math.floor(response.data.wind.speed) + " " + "MPH";
         })
     }
@@ -144,28 +145,30 @@ document.getElementById("button").addEventListener("click", function (event) {
         document.getElementById("newButton").append(newButton);
     }
 
-// Beer funtion
 
+
+
+    // Beer funtion
     function getBeer() {
-        let getBeerURL = "https://api.openbrewerydb.org/breweries?per_page=5&by_city=" + city;
+
+        let getBeerURL = "https://api.openbrewerydb.org/breweries?page=5&per_page=5&by_city=" + city;
         axios.get(getBeerURL).then(function (response) {
+
             let beerData = response.data;
-            console.log(beerData);
             let beerList = [];
+            console.log(beerData);
+
             for (i = 0; i < beerData.length; i++) {
                 let breweryName = response.data[i].name;
                 let breweryAddress = response.data[i].street;
                 let breweryWebsite = response.data[i].website_url;
+               
 
-                let BreweryInfo = "<div> Brewery name: " + breweryName + "<br> Street: " + breweryAddress + "<br> <a href=" + breweryWebsite + ">Visit " + breweryName + " webiste </a><hr></div>";
-            beerList.push(BreweryInfo)
-        }
-                $("#beerDiv").html(beerList);
-            
-
-    
-                
-            
+                let breweryInfo = "<div class='breweryInfo'><h2>" + breweryName + "</h2>" + breweryAddress + "<br> <a class='breweryWebsite' href=" + breweryWebsite + ">www. " + breweryName + " .com </a><br><hr></div>";
+                beerList.push(breweryInfo)
+            }
+            const beerTitle = "<p id='beerTitle'>Breweries you can run to</p><br><hr>";
+            document.getElementById("beerDiv").innerHTML = beerTitle + beerList.join(" ");
         })
     }
 
